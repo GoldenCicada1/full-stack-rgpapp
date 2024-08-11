58e5bfd5fb8db36fa5ee2e32743db99bc905dcd7
Goodluck Japhet <goodluckjaphet7@gmail.com>
2024-08-12 00:29:00 +0300

    add admin template delete admin_panel template

---
+++ admin/src/layout/AppFooter.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+    <div class="layout-footer">
+        RGPAPP
+        <a href="#" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">ADMIN</a>
+    </div>
+</template>
